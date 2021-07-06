1d1d6c751f72f9ad35a966243391fb3e011d46fa
Daniel <danieltagua1967@gmail.com>
2021-07-05 21:08:08 -0300

    va la clase de entidades

--- src/almacenEntidades/Ventas.java
+++ src/almacenEntidades/Ventas.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package almacenEntidades;
 
-/**
- *
- * @author ELSA
- */
 public class Ventas {
+    private Producto profucto;
+    private int precio;
+    private int caantidad;
+
+    public Ventas() {
+    }
+
+    public Ventas(Producto profucto, int precio, int caantidad) {
+        this.profucto = profucto;
+        this.precio = precio;
+        this.caantidad = caantidad;
+    }
+
+    public Producto getProfucto() {
+        return profucto;
+    }
+
+    public void setProfucto(Producto profucto) {
+        this.profucto = profucto;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCaantidad() {
+        return caantidad;
+    }
+
+    public void setCaantidad(int caantidad) {
+        this.caantidad = caantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Ventas{" + "profucto=" + profucto + ", precio=" + precio + ", caantidad=" + caantidad + '}';
+    }
+    
     
 }
